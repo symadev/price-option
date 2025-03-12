5c426ce5b8613e37cb4417abc9aedbef651139be
syma sultana <symasultana2002@gmail.com>
2025-03-12 16:11:55 +0600

    added the nav ber with ancor

---
+++ src/assets/Component/Navber/Navber.jsx
@@ -0,0 +1,29 @@
+import Link from "../Link/Link";
+
+
+
+const Navber = () => {
+
+
+    const routes = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About Us" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/contact", name: "Contact" },
+        { id: 5, path: "/dashboard", name: "Dashboard" }
+      ];
+     
+      
+    return (
+        <nav>
+            <ul className="md:flex">
+            {
+                routes.map(route =><Link  key={route.id} route={route}></Link>)
+                //inside the route we sent the route props
+            }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navber;
